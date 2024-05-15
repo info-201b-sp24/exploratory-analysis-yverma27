572913ecee7da4ad3b7d68a16c308f70b9d8d09b
yverma27 <yverma@uw.edu>
2024-05-15 15:14:48 -0700

    committing line graph to github - yuvika

---
+++ line_graph.R
@@ -0,0 +1,28 @@
+library(ggplot2)
+library(dplyr)
+library(stringr)
+
+dataset <- read.csv("n_movies.csv")
+updated_dataset <- na.omit(dataset)
+
+extracted_year <- str_extract(updated_dataset$year, "(?<=\\()(\\d{4})")
+
+updated_dataset2 <- updated_dataset %>% 
+  mutate(release_year = as.integer(extracted_year))
+
+yearly_frequency <- updated_dataset2 %>%
+  group_by(release_year) %>%
+  summarize(frequency = n())
+
+data_viz <- ggplot(yearly_frequency, aes(x = release_year, y = frequency)) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Amount of Content Released Each Year",
+       x = "Year",
+       y = "Frequency") +
+  theme_minimal() +
+  scale_x_continuous(breaks = seq(1900, 2024, by = 5))
+
+data_viz
+  
+  
