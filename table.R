e137d66f5cacafd486977f1499b2f4b39c701630
yverma27 <yverma@uw.edu>
2024-05-17 15:17:31 -0700

    added table to html - yuvika

--- table.R
+++ table.R
@@ -8,10 +8,9 @@ updated_dataset <- na.omit(dataset)
 arranged_data <- updated_dataset %>% 
   arrange(desc(rating))
 
-arranged_data
-
 selected_data <- arranged_data %>% 
   select(title, year, certificate, genre, rating, votes) %>% 
   head(10)
 
-kable(selected_data, caption = "Top 10 Movies by Rating")
+kable(selected_data, caption = "Top 10 Content on Netflix by Rating")
+
